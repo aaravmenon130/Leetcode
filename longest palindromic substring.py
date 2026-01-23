9f88a7df7d1a07cad818d88f1b6ca675097aec90
aaravmenon130 <aaravmenon130@gmail.com>
2026-01-23 11:58:38 +0530

    Create longest palindromic substring.py

---
+++ longest palindromic substring.py
@@ -0,0 +1,24 @@
+#https://leetcode.com/problems/longest-palindromic-substring/description/
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        res = ''
+        res_len = 0
+        for i in range(n):
+            l, r = i, i
+            while l >= 0 and r < n and s[l] == s[r]:
+                if res_len < r - l + 1:
+                    res_len = r - l + 1
+                    res = s[l : r + 1]
+                l -= 1
+                r += 1
+            
+            l, r = i, i + 1
+            while l >= 0 and r < n and s[l] == s[r]:
+                if r - l + 1 > res_len:
+                    res_len = r - l + 1
+                    res = s[l : r + 1]
+                l -= 1
+                r += 1
+        return res
